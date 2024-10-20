d10cb24a75ac45aea2e4b53b9b13bb8692a09529
Kolakolanikolay <136644874+Kolakolanikolay@users.noreply.github.com>
2024-10-21 02:24:33 +0300

    1st attempt
    3 лаба уже была сделана в курсе... Мне не составит труда ее сделать, если вопрос в этом, некоторую базу по python я имею. Главная проблема лишь в дедлайне, который я и так просрочил... Так что прошу учесть ее как есть... Чтобы дедлайн не давил :(

---
+++ 1_labs/3.py
@@ -0,0 +1,11 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# индекс середины
+middle_index = len(list_players) // 2
+
+first_team = list_players[:middle_index]
+second_team = list_players[middle_index:]
+
+print(first_team)
+print(second_team)
+#P.S. Этот код уже был предоставлен по умолчанию... видимо ошибка, но я ленивый, поэтому оставлю)
