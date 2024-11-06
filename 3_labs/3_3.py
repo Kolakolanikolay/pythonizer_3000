a97c731ee99a7952260b0dec00322cb96945c4fe
Kolakolanikolay <136644874+Kolakolanikolay@users.noreply.github.com>
2024-11-07 00:52:04 +0300

    Update 3_3.py
    исправил 3_3. 
    
    С enumerate для 3_1 не до конца разобрался и решил пропустить. Не вижу ничего критичного в текущем его исполнении

--- 3_labs/3_3.py
+++ 3_labs/3_3.py
@@ -1,43 +1,9 @@
 def count_letters (text):
-    letters_dict={
-        "а": 0,
-        "б": 0,
-        "в": 0,
-        "г": 0,
-        "д": 0,
-        "е": 0,
-        "ё": 0,
-        "ж": 0,
-        "з": 0,
-        "и": 0,
-        "й": 0,
-        "к": 0,
-        "л": 0,
-        "м": 0,
-        "н": 0,
-        "о": 0,
-        "п": 0,
-        "р": 0,
-        "с": 0,
-        "т": 0,
-        "у": 0,
-        "ф": 0,
-        "х": 0,
-        "ц": 0,
-        "ч": 0,
-        "ш": 0,
-        "щ": 0,
-        "ъ": 0,
-        "ы": 0,
-        "ь": 0,
-        "э": 0,
-        "ю": 0,
-        "я": 0,
-    }
+    letters_dict = { }
     text = text.lower()
     for letter in text:
         if letter.isalpha():
-            letters_dict [letter] = letters_dict.get(letter) + 1
+            letters_dict [letter] = letters_dict.setdefault(letter, 0) + 1
     return letters_dict
 
 
