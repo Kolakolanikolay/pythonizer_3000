f9eee3a536d02a240ce30c8b71ecc2aee2b251e6
Kolakolanikolay <136644874+Kolakolanikolay@users.noreply.github.com>
2024-11-24 23:17:05 +0300

    исправлено
    Исправил предыдущие комментарии (хотя до сих пор не пойму, чем там -1 мешала, учитывая то, что я исчерпывающе аргументировал ее постановку)
    
    Также добавил скрин пройденных заданий

--- 2_lab/2_1.py
+++ 2_lab/2_1.py
@@ -3,12 +3,13 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
 
-mes = -1 # так как мы знаем (из учловия), что подушка у нас ЕСТЬ и она положительна
-        # => хотя бы раз в цикл программа точно зайдет. => хотя бы раз mes++
-        # => при изначальном значении mes = -1 программа даст верный ответ
-while money_capital>0:
-    money_capital += (salary - spend)
-    mes+=1
-    spend += spend * increase
-
+mes = 0
+while True:
+    change = salary - spend
+    if change + money_capital < 0:
+        break
+    else:
+        money_capital += change
+        mes += 1
+        spend += (spend* increase)
 print("Количество месяцев, которое можно протянуть без долгов:", mes)
--- 2_lab/2_2.py
+++ 2_lab/2_2.py
@@ -3,11 +3,9 @@ spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
 
-time = months
 money_capital = 0
-while time > 0:
+for i in range (months):
     money_capital -= (salary - spend)
-    spend += spend * increase
-    time -= 1
+    spend +=( spend * increase)
 
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
